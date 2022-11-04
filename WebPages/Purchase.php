6e7ee39bbe580f760f381c51f6c437af31e859c0
PongpunSaengphon <92890500+Pongpun1@users.noreply.github.com>
2022-11-04 20:24:57 +0700

    s

--- WebPages/Purchase.php
+++ WebPages/Purchase.php
@@ -17,27 +17,13 @@
     <link rel="stylesheet" href="Home.css">
     <link rel="stylesheet" href="Purchase.css">
 
-    <style>
-        .btn, .btn:hover {
-            width: 100%;
-            background-color: rgb(241 72 34);
-            border-radius: 20px;
-            border: 2px solid rgb(241 72 34);
-            text-decoration: none;
-        }
-        .modal-title {
-            color: rgb(253 165 39);
-            margin-bottom: 2%;
-        }
-    </style>
-
 </head>
 <body>
     <div class="container-fluid">
-        <div class="header-area">
+    <div class="header-area">
             <div class="header">
-                <img class="logo-img" src="https://cdn.discordapp.com/attachments/1008922955494989965/1033040544206692372/logo.png">
-                <img class="logo-word" src="https://cdn.discordapp.com/attachments/1008922955494989965/1033040544605143090/logo-word.png">
+                <img class="logo-img" src="./IMG/logo/logo-img.png">
+                <img class="logo-word" src="./IMG/logo/logo-word.png">
                 <div class="bar">
                     <a class="line"></a>
                     <a class="nav" href="Home.html" style="left: 235px; color: rgb(253 165 39);">หน้าหลัก</a>
@@ -46,7 +32,7 @@
                 </div>
                 <div class="bar-basket">
                     <a class="basket-bg"></a>
-                    <img class="logo-basket" src="https://cdn.discordapp.com/attachments/1008922955494989965/1033047007151013888/logo-basket.png">
+                    <img class="logo-basket" src="./IMG/logo/logo-basket.png">
                     <a class="nav" href="Purchase.php" style="right: 95px; color: white;">ออเดอร์ของคุณ</a>
                     <a class="basket-num"><p style="color: white;">55</p></a>
                 </div>
@@ -62,7 +48,7 @@
                         </div>
                     </div>
                 </div>
-                <img class="logo-user" href="User.html" src="https://cdn.discordapp.com/attachments/1008922955494989965/1033046179614830622/logo-user.png">
+                <img class="logo-user" href="User.html" src="./IMG/logo/logo-user.png">
             </div>
         </div>
 
@@ -87,123 +73,25 @@
                 <th>ราคา</th>
             </tr>
             <?php
-                // $sql ="SELECT * from selectMenu";
-                // $ret = $db->query($sql);
-                // while($row = $ret->fetchArray(SQLITE3_ASSOC) ) {
-                //     echo "<tr>";
-                //     echo "<td>" . $row['Image'] . "</td>" . $row['Menu'] . "</td>";
-                //     echo "<td>" . $row['Number'] . "</td>";
-                //     echo "<td>" . (+$row['Price'])*(+$row['Number']) . "</td>";
-                //     echo "</tr>";
-                // }
+                $sql ="SELECT * from selectMenu";
+                $ret = $db->query($sql);
+                while($row = $ret->fetchArray(SQLITE3_ASSOC) ) {
+                    echo "<tr>";
+                    echo "<td>" . $row['Image'] . "</td>" . $row['Menu'] . "</td>";
+                    echo "<td>" . $row['Number'] . "</td>";
+                    echo "<td>" . (+$row['Price'])*(+$row['Number']) . "</td>";
+                    echo "</tr>";
+                }
             ?>
             </table>
-        </div>
-        <div class="purchase">
-            <div class="card text-center" style="width: 15rem;">
-                <div class="card-body">
-                    <p class="card-text">
-                        <span style="float: left;">ค่าอาหาร</span>
-                        <span style="float: right;">0 บาท</span><br>
-                        <span style="float: left;">ค่าจัดส่ง</span>
-                        <span style="float: right;">0 บาท</span><br><br>
-                        <b><span style="float: left;">ยอดรวม</span>
-                        <span style="float: right;">0 บาท</span><br></b>
-                    </p>
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#orderConfirm">ดำเนินการต่อ</button>
-                </div>
-            </div>
-        </div>
+            <div class="purchase">
 
-        <div class="modal fade" id="orderConfirm" tabindex="-1" role="dialog" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-body">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                        <h5 class="modal-title"><b>ที่อยู่สำหรับการจัดส่ง</b></h5>
-                        <form>
-                            <div class="row">
-                                <div class="col">
-                                    <div class="input-group mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text">ชื่อ</span>
-                                        </div>
-                                        <input type="text" class="form-control" aria-label="fname">
-                                    </div>
-                                </div>
-                                <div class="col">
-                                    <div class="input-group mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text">นามสกุล</span>
-                                        </div>
-                                        <input type="text" class="form-control" aria-label="lname">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="input-group mb-3">
-                                <div class="input-group-prepend" >
-                                    <span class="input-group-text">ที่อยู่</span>
-                                </div>
-                                <textarea class="form-control rounded-0" rows="2"></textarea>
-                            </div>
-                            <div class="row">
-                                <div class="col">
-                                    <div class="input-group mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text">ตำบล/แขวง</span>
-                                        </div>
-                                        <input type="text" class="form-control" aria-label="district">
-                                    </div>
-                                </div>
-                                <div class="col">
-                                    <div class="input-group mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text">จังหวัด</span>
-                                        </div>
-                                        <input type="text" class="form-control" aria-label="province" >
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col">
-                                    <div class="input-group mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text">รหัสไปรษณีย์</span>
-                                        </div>
-                                        <input type="text" class="form-control" aria-label="postnum">
-                                    </div>
-                                </div>
-                                <div class="col">
-                                    <div class="input-group mb-3">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text">เบอร์โทรศัพท์</span>
-                                        </div>
-                                        <input type="text" class="form-control" aria-label="phone">
-                                    </div>
-                                </div>
-                            </div>
-                            <h5 class="modal-title" style="margin-bottom: 1%;"><b>รูปแบบการชำระเงิน</b></h5>
-                            <div class="form-check" style="margin-left: 5%;">
-                                <input class="form-check-input" type="radio" name="payment">
-                                <label class="form-check-label">เก็บเงินปลายทาง</label><br>
-                                <input class="form-check-input" type="radio" name="payment">
-                                <label class="form-check-label">บัญชีธนาคาร</label>
-                            </div>
-                        </form>
-
-                        <button type="button" class="btn btn-primary">ดำเนินการต่อ</button>
-
-                    </div>
-                </div>
             </div>
         </div>
 
         <?php
             $db->close();
         ?>
-
     </div>
 </body>
 </html>
