435df0a19295220fb1d1dbafc8d6a56d19791eac
llPornpilin <linglingppl@gmail.com>
2022-11-06 17:46:19 +0700

    change bootstrap ver. to 4.3.1 and modify modal size in sign in, sign up page

--- WebPages/SignUp.php
+++ WebPages/SignUp.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register SignUp</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
@@ -59,7 +59,7 @@
     </div>
 
 
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog modal-xl">
         <div class="modal-content">
             <form class="form-row" action="profile.php" method="POST">
                 <div class="left">
